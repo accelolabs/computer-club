9245872ce5cb2c3f53bf3a37fa391d1179e63608
accelolabs <accelotron@niuitmo.ru>
2025-05-04 05:25:52 +0300

    feat: you can actually create events now

--- src/Events.h
+++ src/Events.h
@@ -1,40 +1,145 @@
 #pragma once
 
 #include <variant>
+#include "Time.h"
 
 
 struct ClientArrivedEvent {
+    
+    Time time;
+
     int id = 1;
+
+    std::string client_name;
+
+
+    ClientArrivedEvent() = delete;
+
+    ClientArrivedEvent(const Time& time, const std::string& client_name) :
+        time(time),
+        client_name(client_name)
+    {}
+
 };
 
 
 struct ClientSatEvent {
+    
+    Time time;
+
     int id = 1;
+
+    std::string client_name;
+
+    int table_id;
+
+
+    ClientSatEvent() = delete;
+
+    ClientSatEvent(const Time& time, const std::string& client_name, int table_id) :
+        time(time),
+        client_name(client_name),
+        table_id(table_id)
+    {}
+
 };
 
 
 struct ClientWaitedEvent {
+    
+    Time time;
+
     int id = 3;
+
+    std::string client_name;
+
+
+    ClientWaitedEvent() = delete;
+
+    ClientWaitedEvent(const Time& time, const std::string& client_name) :
+        time(time),
+        client_name(client_name)
+    {}
+
 };
 
 
 struct ClientLeftEvent {
+    
+    Time time;
+
     int id = 4;
+
+    std::string client_name;
+
+
+    ClientLeftEvent() = delete;
+
+    ClientLeftEvent(const Time& time, const std::string& client_name) :
+        time(time),
+        client_name(client_name)
+    {}
+
 };
 
 
 struct ClientKickedEvent {
+    
+    Time time;
+
     int id = 11;
+
+    std::string client_name;
+
+
+    ClientKickedEvent() = delete;
+
+    ClientKickedEvent(const Time& time, const std::string& client_name) :
+        time(time),
+        client_name(client_name)
+    {}
+
 };
 
 
 struct QueuedClientSatEvent {
+    
+    Time time;
+
     int id = 12;
+
+    std::string client_name;
+
+    int table_id;
+
+
+    QueuedClientSatEvent() = delete;
+
+    QueuedClientSatEvent(const Time& time, const std::string& client_name, int table_id) :
+        time(time),
+        client_name(client_name),
+        table_id(table_id)
+    {}
+
 };
 
 
 struct ErrorEvent {
+    
+    Time time;
+
     int id = 13;
+
+    std::string error_name;
+
+
+    ErrorEvent() = delete;
+
+    ErrorEvent(const Time& time, const std::string& error_name) :
+        time(time),
+        error_name(error_name)
+    {}
+
 };
 
 
