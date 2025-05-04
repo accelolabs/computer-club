be6673c2c6d6f827b18e45c4539bf99dbf8f1c56
accelolabs <accelotron@niuitmo.ru>
2025-05-04 19:41:13 +0300

    feat: tables

---
+++ src/model/Table.cpp
@@ -0,0 +1,30 @@
+#include "Table.h"
+
+
+bool Table::is_occupied() const {
+    return occupied;
+}
+
+
+std::string Table::occupied_by() const {
+    return occupier;
+}
+
+
+void Table::occupy(const Time& time, const std::string& client_name) {
+    if (occupied) return;
+
+    occupied = true;
+    occupier = client_name;
+    session_start = time;
+}
+
+void Table::free(const Time& time, int price) {
+    if (!occupied) return;
+
+    occupied = false;
+
+    int minutes_occupied = time.get_total_minutes() - session_start.get_total_minutes();
+    revenue += (minutes_occupied / 60) * price;
+    minutes += minutes_occupied;
+}
---
+++ src/model/Table.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Time.h"
+#include <string>
+
+
+class Table {
+
+    private:
+
+    int id;
+
+    std::string occupier;
+
+    int revenue = 0;
+
+    int minutes = 0;
+
+    Time session_start {"00:00"};
+
+    bool occupied = false;
+
+
+    public:
+
+    Table(int id) : id(id) {}
+
+
+    bool is_occupied() const;
+
+
+    std::string occupied_by() const;
+
+
+    void occupy(const Time& time, const std::string& client_name);
+    
+    void free(const Time& time, int price);
+
+};
