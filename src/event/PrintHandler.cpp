5e740a6f8edff964d081d6a1c7c9eb87f755da88
accelolabs <accelotron@niuitmo.ru>
2025-05-04 17:51:59 +0300

    feat: printing events to any ostream

---
+++ src/event/PrintHandler.cpp
@@ -0,0 +1,56 @@
+#include "PrintHandler.h"
+
+
+PrintHandler::PrintHandler(std::ostream& output) : output(output) {}
+
+
+void PrintHandler::operator()(const ClientArrivedEvent& event) {
+    output
+        << event.time.as_string() << " "
+        << event.id << " "
+        << event.client_name << "\n";
+}
+
+void PrintHandler::operator()(const ClientSatEvent& event) {
+    output
+        << event.time.as_string() << " "
+        << event.id << " "
+        << event.client_name << " "
+        << event.table_id << "\n";
+}
+
+void PrintHandler::operator()(const ClientWaitedEvent& event) {
+    output
+        << event.time.as_string() << " "
+        << event.id << " "
+        << event.client_name << "\n";
+}
+
+void PrintHandler::operator()(const ClientLeftEvent& event) {
+    output
+        << event.time.as_string() << " "
+        << event.id << " "
+        << event.client_name << "\n";
+}
+
+void PrintHandler::operator()(const ClientKickedEvent& event) {
+    output
+        << event.time.as_string() << " "
+        << event.id << " "
+        << event.client_name << "\n";
+}
+
+void PrintHandler::operator()(const QueuedClientSatEvent& event) {
+    output
+        << event.time.as_string() << " "
+        << event.id << " "
+        << event.client_name << " "
+        << event.table_id << "\n";
+}
+
+void PrintHandler::operator()(const ErrorEvent& event) {
+    output
+        << event.time.as_string() << " "
+        << event.id << " "
+        << event.error_name << "\n";
+}
---
+++ src/event/PrintHandler.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Events.h"
+
+
+class PrintHandler {
+
+private:
+
+    std::ostream& output;
+
+public:
+
+    PrintHandler(std::ostream& output);
+
+
+    void operator()(const ClientArrivedEvent& event);
+    
+    void operator()(const ClientSatEvent& event);
+    
+    void operator()(const ClientWaitedEvent& event);
+    
+    void operator()(const ClientLeftEvent& event);
+    
+    void operator()(const ClientKickedEvent& event);
+    
+    void operator()(const QueuedClientSatEvent& event);
+
+    void operator()(const ErrorEvent& event);
+
+};
